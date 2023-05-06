b22906810c01ccddbcd9323af512a87d97c8ebfb
Sachin Verma <83693262+iam-sachinverma@users.noreply.github.com>
2023-05-06 11:39:47 +0530

    first commit 🚀

---
+++ app/layout.tsx
@@ -0,0 +1,21 @@
+import '../styles/globals.scss'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'EventEngage Inc',
+  description: 'Front End Developer Role Assignment',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
