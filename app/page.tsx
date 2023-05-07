edcf10ca05aaff39d81345a7e98580704e60b2f6
Sachin Verma <83693262+iam-sachinverma@users.noreply.github.com>
2023-05-07 08:35:53 +0530

    Banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,28 @@
 "use client";
 
+import { Button } from "antd";
 import Image from "next/image";
 import styles from "./page.module.scss";
 
+import Banner from "@/components/Banner/banner";
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <main className={styles.container}>
+      <div className={styles.page_top}>
+        <div className={styles.heading_1}>
+          <h1>WeWork + Salesforce</h1>
+        </div>
+        <Button
+          style={{ backgroundColor: "#00B686", borderColor: "#00B686" }}
+          type="primary"
+        >
+          Publish/Share
+        </Button>
+      </div>
+      <div>
+        <Banner />
+      </div>
+    </main>
+  );
 }
---
+++ components/Banner/banner.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import styles from "./banner.module.scss";
+import Image from "next/image";
+
+export default function Banner() {
+  return (
+    <div className={styles.banner}>
+      <div className={styles.desktop}>
+        <Image src="/banner.png" alt="banner" fill quality={100} />
+      </div>
+      <div className={styles.medium}>
+        <Image src="/medium_banner.png" alt="banner" fill quality={100} />
+      </div>
+      <div className={styles.mobile}>
+        <Image src="/small_banner.png" alt="banner" fill quality={100} />
+      </div>
+
+      <div className={styles.banner_overlay}></div>
+      <div className={styles.banner_content}>
+        <h1>
+          Workspace <br /> Proposal
+        </h1>
+      </div>
+    </div>
+  );
+}
