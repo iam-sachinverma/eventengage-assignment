3664f0433c6ea992d708393d72a33e0d05779665
Sachin Verma <83693262+iam-sachinverma@users.noreply.github.com>
2023-05-08 07:21:47 +0530

    Banner Overlay -- FIX

--- components/Banner/banner.tsx
+++ components/Banner/banner.tsx
@@ -5,24 +5,25 @@ import Image from "next/image";
 
 export default function Banner() {
   return (
-    <div className={styles.banner}>
-      <div className={styles.desktop}>
-        <Image src="/banner.png" alt="banner" fill quality={100} />
-      </div>
-      <div className={styles.medium}>
-        <Image src="/medium_banner.png" alt="banner" fill quality={100} />
-      </div>
-      <div className={styles.mobile}>
-        <Image src="/small_banner.png" alt="banner" fill quality={100} />
-      </div>
+    <div className={styles.wrapper}>
+      <div className={styles.banner}>
+        <div className={styles.desktop}>
+          <Image src="/banner.png" alt="banner" fill quality={100} />
+        </div>
+        <div className={styles.medium}>
+          <Image src="/medium_banner.png" alt="banner" fill quality={100} />
+        </div>
+        <div className={styles.mobile}>
+          <Image src="/small_banner.png" alt="banner" fill quality={100} />
+        </div>
 
-      <div className={styles.banner_overlay}></div>
-      <div className={styles.banner_content}>
-        <h1>
-          Workspace <br /> Proposal
-        </h1>
+        <div className={styles.banner_overlay}></div>
+        <div className={styles.banner_content}>
+          <h1>
+            Workspace <br /> Proposal
+          </h1>
+        </div>
       </div>
-
       <div className={styles.brand_logo}>
         <div className={styles.logo_1}>
           <Image
